166d89f8f1a8c9cb758e36223deda1903ef03924
Hana Dusíková <hanicka@hanicka.net>
2023-01-22 00:28:09 +0100

    update example

--- example.cpp
+++ example.cpp
@@ -2,10 +2,16 @@
 #include <iostream>
 
 int main(int argc, char ** argv) {
-	std::cout << "    sha224 = " << cthash::sha224{}.update(std::string_view(argv[1])).final() << "\n";
-	std::cout << "    sha256 = " << cthash::sha256{}.update(std::string_view(argv[1])).final() << "\n";
-	std::cout << "    sha384 = " << cthash::sha384{}.update(std::string_view(argv[1])).final() << "\n";
-	std::cout << "    sha512 = " << cthash::sha512{}.update(std::string_view(argv[1])).final() << "\n";
-	std::cout << "sha512/224 = " << cthash::sha512t<224>{}.update(std::string_view(argv[1])).final() << "\n";
-	std::cout << "sha512/256 = " << cthash::sha512t<256>{}.update(std::string_view(argv[1])).final() << "\n";
+	if (argc < 2) {
+		return 1;
+	}
+
+	const auto in = std::string_view(argv[1]);
+
+	std::cout << "    sha224 = " << cthash::sha224{}.update(in).final() << "\n";
+	std::cout << "    sha256 = " << cthash::sha256{}.update(in).final() << "\n";
+	std::cout << "    sha384 = " << cthash::sha384{}.update(in).final() << "\n";
+	std::cout << "    sha512 = " << cthash::sha512{}.update(in).final() << "\n";
+	std::cout << "sha512/224 = " << cthash::sha512t<224>{}.update(in).final() << "\n";
+	std::cout << "sha512/256 = " << cthash::sha512t<256>{}.update(in).final() << "\n";
 }
