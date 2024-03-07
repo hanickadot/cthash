7d1b3a9a9617345f8807117aa0fff063f916fec5
Hana Dusíková <hanicka@hanicka.net>
2024-03-07 13:46:35 +0100

    add sha256 example

---
+++ examples/sha256.cpp
@@ -0,0 +1,10 @@
+#include <cthash/sha2/sha256.hpp>
+#include <print>
+
+using namespace cthash::literals;
+
+int main() {
+	constexpr auto a = cthash::sha256{}.update("hello there!").final();
+
+	std::print("{:hexdec}", a);
+}
