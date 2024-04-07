8a2f9cbd47f708584b1fdc06a381345724c8d2da
Hana Dusíková <hanicka@hanicka.net>
2024-04-07 08:43:19 +0200

    Update sha3.cpp

--- examples/sha3.cpp
+++ examples/sha3.cpp
@@ -1,10 +1,10 @@
-#include <cthash/sha2/sha256.hpp>
+#include <cthash/sha3/sha3-256.hpp>
 #include <print>
 
 using namespace cthash::literals;
 
 int main() {
-	constexpr auto a = cthash::sha256{}.update("hello there!").final();
+	constexpr auto a = cthash::sha3_256{}.update("hello there!").final();
 
-	std::print("{:HEXDEC}", a);
+	std::print("{:base32}", a);
 }
